64aef8b72c6c8af462cf8e98fa1231adc0ad2035
José Guerrero Gallego <jguegal@outlook.com>
2021-02-27 16:32:17 +0100

    Start transition to Single App Page

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html'
-      $(this).load(file)
-    })
+/**$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+	var file = 'views/' + $(this).data('include') + '.html'
+	$(this).load(file)
   })
+})**/
+
+const showOrHideSection = (section) => {
+	const container = document.getElementById(section)
+	container.style.display === "none" ? container.style.display = "block" : container.style.display = "none"
+		
+}
