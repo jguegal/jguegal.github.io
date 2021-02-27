427f4281b2e2a47161d7af4464e9f34c09b2bd9f
José Guerrero Gallego <jguegal@outlook.com>
2021-02-27 21:02:51 +0100

    Incluided aboutme view

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-/**$(function () {
-  var includes = $('[data-include]')
-  $.each(includes, function () {
-	var file = 'views/' + $(this).data('include') + '.html'
-	$(this).load(file)
-  })
-})**/
-
 const backToGreeter = () => {
 	let container = document.getElementById("projects-container")
 	container.style.display = "none"
-	//container = document.getElementById("aboutme-container")
-	//container.style.display = "none"
+	container = document.getElementById("aboutme-container")
+	container.style.display = "none"
 
 	container = document.getElementById("greeter-container")
 	container.style.display = "block"
-
 }
 
 const hideGreeter = () => {
